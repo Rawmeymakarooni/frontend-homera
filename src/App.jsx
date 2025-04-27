5aea69d433c1060d87b5d3962c86c912c2ad34bb
sskiarh <saskiirahmaa@gmail.com>
2025-04-27 18:24:24 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
 import Navbar from './assets/Navbar';
 import Hero from './assets/Hero';
+import './index.css';
+import './App.css'; 
 
 function App() {
   const [count, setCount] = useState(0)
